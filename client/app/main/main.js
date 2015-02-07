2270960f82f39f93b55a1d705cb504644cd3b55a
teleaziz <abdz.abbas@gmail.com>
2015-02-07 13:57:09 -0800

    First Commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('unstuckMeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
